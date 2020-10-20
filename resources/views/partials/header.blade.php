b5bb3d411d3fd8d579f8546b43a90d2d00b9812d
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-10-20 15:41:02 +0200

    iniziato front-end

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,18 +1,22 @@
 {{-- HEADER --}}
 <header>
+    {{-- header-top --}}
     <div class="header-top">
         <div class="logo-wrapper">
         <img src="{{asset('images/marchio-sito-test.png')}}" alt="">
         </div>
     </div>
+    {{-- /header-top --}}
+    {{-- header-bottom --}}
     <div class="header-bottom">
-        <nav class="main-nav">
-            <ul class="menu-list">
+        <nav class="header-nav">
+            <ul class="header-nav-list">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Prodotti</a></li>
                 <li><a href="#">News</a></li>
             </ul>
         </nav>
     </div>
+    {{-- /header-bottom --}}
 </header>
 {{-- /HEADER --}}

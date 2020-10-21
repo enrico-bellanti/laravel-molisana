ce56569a4c8e0516909bd50af51ed5365b18e82f
Enrico Bellanti <enrico.bellanti@gmail.com>
2020-10-21 15:14:27 +0200

    implementato products

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,5 +1,25 @@
 @extends('layouts.layout-main') 
 
 @section('mainContent')
-    <h1>I Prodotti</h1>
+    {{-- MAIN --}}
+    <main>
+        <div class="main-wrapper">
+            @if (!empty($products))
+                @foreach ($products as $key => $type)
+                    <h2>{{$key}}</h2>
+                    <ul class="products">
+                        @foreach ($type as $product)
+                        <li class="product">
+                            <img src="{{$product["src"]}}" alt="">
+                            <div class="product-layout"></div>
+                            <a class="product-name" href="#"><h3>{{$product["titolo"]}}</h3></a>
+                            <a class="icon-cutlery" href="#"><img src="{{asset('images/icon.svg')}}" alt=""></a>
+                        </li>
+                        @endforeach
+                    </ul>
+                @endforeach
+            @endif
+        </div>
+    </main>
+    {{-- /MAIN --}}
 @endsection
